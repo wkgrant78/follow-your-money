05208b4249ba491a0e6d197e00d7432ef83b382c
Wendy Grant <wendykgrant.tx@gmail.com>
2020-11-17 17:23:24 -0600

    finished manifest/service-worker updated server/db

--- public/db.js
+++ public/db.js
@@ -1,69 +1,74 @@
-et db;
-// create a new db request for a "budget" database.
-const request = indexedDB.open("budget", 1);
+const FILES_TO_CACHE = [
+    '/index.html',
+    '/favicon.ico',
+    '/manifest.webmanifest',
+    '/dist/index.js',
+    '/dist/style.css',
+    '/dist/db.js',
+];
 
-request.onupgradeneeded = function(event) {
-   // create object store called "pending" and set autoIncrement to true
-  const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
-};
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-request.onsuccess = function(event) {
-  db = event.target.result;
 
-  // check if app is online before reading from db
-  if (navigator.onLine) {
-    checkDatabase();
-  }
-};
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
 
-request.onerror = function(event) {
-  console.log("Woops! " + event.target.errorCode);
-};
+    self.skipWaiting();
+});
 
-function saveRecord(record) {
-  // create a transaction on the pending db with readwrite access
-  const transaction = db.transaction(["pending"], "readwrite");
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
 
-  // access your pending object store
-  const store = transaction.objectStore("pending");
+    self.clients.claim();
+});
 
-  // add record to your store with add method.
-  store.add(record);
-}
+// fetch
+self.addEventListener("fetch", function (evt) {
+    // cache successful requests to the API
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
 
-function checkDatabase() {
-  // open a transaction on your pending db
-  const transaction = db.transaction(["pending"], "readwrite");
-  // access your pending object store
-  const store = transaction.objectStore("pending");
-  // get all records from store and set to a variable
-  const getAll = store.getAll();
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
+        );
 
-  getAll.onsuccess = function() {
-    if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
-      })
-      .then(response => response.json())
-      .then(() => {
-        // if successful, open a transaction on your pending db
-        const transaction = db.transaction(["pending"], "readwrite");
-
-        // access your pending object store
-        const store = transaction.objectStore("pending");
-
-        // clear all items in your store
-        store.clear();
-      });
+        return;
     }
-  };
-}
 
-// listen for app coming back online
-window.addEventListener("online", checkDatabase);
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+    evt.respondWith(
+        caches.match(evt.request).then(function (response) {
+            return response || fetch(evt.request);
+        })
+    );
+});
--- server.js
+++ server.js
@@ -28,8 +28,4 @@ app.listen(PORT, () => {
 });
 
 
-db.newdatabase.insert({
-  "name2": "this is name2",
-  "title": ["item 1", "item2", "item3"],
-  "country": "this is country"
-})
+
